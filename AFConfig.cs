cc610503d612113eecb2081c9cde54731f47cae6
Lina Grundvall <lina.grundvall@learnet.se>
2021-12-27 11:39:19 +0100

    Add project files.

---
+++ AFConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace Design_Patterns_Assignment
+{
+    public class AFConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register Main Application
+            //builder.RegisterType<Application>().As<IApplication>();
+
+            //Register Coffee
+            //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            //    .Where(i => i.Namespace.Contains("Decorators"))
+            //    .As(i => i.GetInterfaces()
+            //    .FirstOrDefault(n => n.Name == "I" + i.Name));
+
+
+            //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            //    .Where(i => i.Namespace.Contains("Coffee"))
+            //    .As(i => i.GetInterfaces()
+            //    .FirstOrDefault(n => n.Name == "I" + i.Name));
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Design_Patterns_Assignment;
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Run Autofac Configure
+            var container = AFConfig.Configure();
+
+            Decorator.Run();
+            Repository.Run();
+            Strategy.Run();
+            Observer.Run();
+        }     
+    }
+}
